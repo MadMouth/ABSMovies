253261682c28430822da2401f7d07d68d29423d6
MadMouth <madmouth4@gmail.com>
2022-03-10 16:47:13 +0300

    init commit

---
+++ src/main/java/ABSMovies/AbsMoviesApplication.java
@@ -0,0 +1,13 @@
+package ABSMovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbsMoviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ABSMovies/AbsMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package ABSMovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbsMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
