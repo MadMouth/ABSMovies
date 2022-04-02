44cd9796144719732bf5625fa2ddc34fbb6abfa2
MadMouth <madmouth4@gmail.com>
2022-04-02 16:49:08 +0300

    update src file

--- src/main/java/ABSMovies/controller/FavoriteController.java
+++
@@ -1,9 +0,0 @@
-package ABSMovies.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/v1/favorite")
-public class FavoriteController {
-}
--- src/main/java/ABSMovies/service/FavoriteUser.java
+++
@@ -1,9 +0,0 @@
-package ABSMovies.service;
-
-
-import org.springframework.stereotype.Service;
-
-//save favorite of user
-@Service
-public class FavoriteUser {
-}
